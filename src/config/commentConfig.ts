251661160716a776513e13a347726773349d475d
hamster-yhz <1427739483@qq.com>
2025-10-23 16:22:29 +0800

    feat: 设置评论系统链接

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -4,7 +4,7 @@ export const commentConfig: CommentConfig = {
   enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
   enableVisitorCount: true, // 启用文章访问量统计功能。当设置为 false 时，文章访问量统计将不会显示。需要enable和enableVisitorCount都为true时才启用。
   twikoo: {
-    envId: "https://twikoo.vercel.app",
+    envId: "https://hamster-yhz-myblog.hf.space",
     lang: "zh-CN", // 设置 Twikoo 评论系统语言
   },
 };
