bd5b992af69579b8a87ef951a73ac95533cb299f
hamster-yhz <1427739483@qq.com>
2025-10-23 12:40:09 +0800

    config: 友链编辑

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -4,41 +4,79 @@ import type { FriendLink } from "../types/config";
 
 // 友链配置
 export const friendsConfig: FriendLink[] = [
+  // 博客
   {
-    title: "夏夜流萤",
+    title: "黑盒 Blackbox",
     imgurl:
-      "https://q.qlogo.cn/headimg_dl?dst_uin=7618557&spec=640&img_type=jpg",
+      "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711598888_blackbox.png",
     desc: "总有一场相遇，是互相喜欢的！",
-    siteurl: "https://blog.cuteleaf.cn",
+    siteurl: "http://blackbox.655353.xyz/",
     tags: ["Blog"],
-    weight: 10, // 权重，数字越大排序越靠前
+    weight: 100, // 权重，数字越大排序越靠前
     enabled: true, // 是否启用
   },
+  // 服务监控
   {
-    title: "Firefly Docs",
-    imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-    desc: "Firefly主题模板文档",
-    siteurl: "https://docs-firefly.cuteleaf.cn",
-    tags: ["Docs"],
-    weight: 9,
+    title: "Hawkeye Surveillance",
+    imgurl:
+      "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711665229_hawkeye-909333.png",
+    desc: "服务监控面板，系统可观测与告警",
+    siteurl: "https://hawkeye.909333.xyz/",
+    tags: ["Monitoring", "Ops"],
+    weight: 90,
     enabled: true,
   },
+  // 小鸡探针（探针面板）
   {
-    title: "Firefly",
-    imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-    desc: "Firefly 一款清新美观的 Astro 博客主题模板",
-    siteurl: "https://github.com/CuteLeaf/Firefly",
-    tags: ["GitHub", "Theme"],
-    weight: 9,
+    title: "小鸡探针",
+    imgurl:
+      "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711587838_hawkeye-655353.png",
+    desc: "服务器探针监控面板",
+    siteurl: "https://hawkeye.655353.xyz/",
+    tags: ["Monitoring", "Probe"],
+    weight: 85,
     enabled: true,
   },
+  // 图床
   {
-    title: "Astro",
-    imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-    desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-    siteurl: "https://github.com/withastro/astro",
-    tags: ["Framework"],
-    weight: 8,
+    title: "yuanshen 图床",
+    imgurl:
+      "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711678393_yuanshen-image.png",
+    desc: "个人图片托管与图床服务",
+    siteurl: "http://yuanshen-image.909333.xyz/",
+    tags: ["Tools", "Image"],
+    weight: 80,
+    enabled: true,
+  },
+  // 邮箱服务
+  {
+    title: "yuanshenMail",
+    imgurl:
+      "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711676572_yuanshen-mail.png",
+    desc: "邮箱服务",
+    siteurl: "https://mail.909333.xyz/",
+    tags: ["Tools", "Mail"],
+    weight: 70,
+    enabled: true,
+  },
+  // 游戏：中国象棋
+  {
+    title: "中国象棋",
+    imgurl: "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711555383_chinese-chess.png",
+    desc: "在线对弈，实时对战 / 简洁 UI / 轻量部署",
+    siteurl: "http://chinese-chess.655353.xyz/",
+    tags: ["Game"],
+    weight: 60,
+    enabled: true,
+  },
+  // 游戏：谜题小游戏
+  {
+    title: "谜题小游戏",
+    imgurl: "https://yuanshen-image.909333.xyz/file/default/hamster-yhz/readme/1760711670561_stats-fallback.png",
+    desc: "关卡化 / 静态托管 / 低成本运维",
+    siteurl: "http://qushizitiaozhan.655353.xyz/",
+    tags: ["Game"],
+    weight: 50,
     enabled: true,
   },
 ];
