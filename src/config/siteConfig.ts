fc4a33e759be64570284e0c3e576a55dd748f418
hamster-yhz <1427739483@qq.com>
2025-10-23 12:43:58 +0800

    config: 站点信息编辑

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -5,15 +5,14 @@ import { fontConfig } from "./fontConfig";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
-  title: "Firefly",
-  subtitle: "Demo site",
+  title: "MyBlog",
+  subtitle: "-- hamster-yhz",
   description:
-    "Firefly 是一款基于 Astro 框架开发的清新美观且现代化个人博客主题，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+    "一个记录生活与技术的博客，欢迎来到哈基米的世界！",
   keywords: [
-    "Firefly",
-    "Fuwari",
+    "yblog",
+    "hamster-yhz",
     "Astro",
-    "ACGN",
     "博客",
     "技术博客",
     "静态博客",
